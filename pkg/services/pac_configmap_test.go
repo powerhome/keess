1cf61c12fbb5e824cfef144203d9724f4669f6d1
Marcus Leandro <mvleandro@gmail.com>
2024-02-09 10:23:16 -0300

    Adding more tests.

---
+++ pkg/services/pac_configmap_test.go
@@ -0,0 +1,152 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	core "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func ConfigMapTestPrepare(t *testing.T) {
+	originalConfigMap := core.ConfigMap{
+		ObjectMeta: v1.ObjectMeta{
+			Namespace:       "original-ns",
+			UID:             "original-uid",
+			ResourceVersion: "123",
+			Annotations: map[string]string{
+				"SomeOtherAnnotation": "value",
+			},
+			Labels: map[string]string{
+				"SomeOtherLabel": "value",
+			},
+		},
+	}
+	pacConfigMap := PacConfigMap{
+		ConfigMap: &originalConfigMap,
+		Cluster:   "test-cluster",
+	}
+
+	newNamespace := "new-ns"
+	preparedConfigMap := pacConfigMap.Prepare(newNamespace)
+
+	// Verifica se o namespace foi atualizado corretamente
+	assert.Equal(t, newNamespace, preparedConfigMap.Namespace, "Namespace should be updated")
+
+	// Verifica se o UID foi limpo
+	assert.Empty(t, preparedConfigMap.UID, "UID should be empty")
+
+	// Verifica se as labels foram definidas corretamente
+	assert.Equal(t, map[string]string{ManagedLabelSelector: "true"}, preparedConfigMap.Labels, "Labels should be correctly set")
+
+	// Verifica se as annotations foram definidas corretamente
+	expectedAnnotations := map[string]string{
+		SourceClusterAnnotation:         "test-cluster",
+		SourceNamespaceAnnotation:       "original-ns",
+		SourceResourceVersionAnnotation: "123",
+	}
+	assert.Equal(t, expectedAnnotations, preparedConfigMap.Annotations, "Annotations should be correctly set")
+
+	// Verifica se o ResourceVersion foi limpo
+	assert.Empty(t, preparedConfigMap.ResourceVersion, "ResourceVersion should be empty")
+}
+
+func ConfigMapTestHasChanged(t *testing.T) {
+
+	localConfigMap := &core.ConfigMap{
+		ObjectMeta: v1.ObjectMeta{
+			ResourceVersion: "1",
+			Namespace:       "local-ns",
+		},
+	}
+	pacConfigMap := PacConfigMap{
+		ConfigMap: localConfigMap,
+		Cluster:   "local-cluster",
+	}
+
+	tests := []struct {
+		name     string
+		remote   *core.ConfigMap
+		expected bool
+	}{
+		{
+			name: "Different ResourceVersion",
+			remote: &core.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Annotations: map[string]string{
+						SourceResourceVersionAnnotation: "2",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "ManagedLabelSelector not true",
+			remote: &core.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Annotations: map[string]string{
+						SourceResourceVersionAnnotation: "1",
+					},
+					Labels: map[string]string{
+						ManagedLabelSelector: "false",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "Different SourceClusterAnnotation",
+			remote: &core.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Labels: map[string]string{
+						ManagedLabelSelector: "true",
+					},
+					Annotations: map[string]string{
+						SourceResourceVersionAnnotation: "1",
+						SourceClusterAnnotation:         "remote-cluster",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "Different SourceNamespaceAnnotation",
+			remote: &core.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Labels: map[string]string{
+						ManagedLabelSelector: "true",
+					},
+					Annotations: map[string]string{
+						SourceResourceVersionAnnotation: "1",
+						SourceClusterAnnotation:         "local-cluster",
+						SourceNamespaceAnnotation:       "remote-ns",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "No Changes",
+			remote: &core.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Annotations: map[string]string{
+						SourceResourceVersionAnnotation: "1",
+						SourceClusterAnnotation:         "local-cluster",
+						SourceNamespaceAnnotation:       "local-ns",
+					},
+					Labels: map[string]string{
+						ManagedLabelSelector: "true",
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := pacConfigMap.HasChanged(test.remote)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
