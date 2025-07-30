b7b246811647c94df92748c25cba5ea43dd67f79
Diego Morales <dgmorales@gmail.com>
2025-07-30 17:39:59 -0300

    Revert "update version"
    This reverts commit ee25d249c37c6fd911892c00279e4c15a5b5b349.

--- cmd/version.go
+++ cmd/version.go
@@ -7,12 +7,12 @@ import (
 )
 
 // Version of the application, set this variable during build
-var version = "1.3.0"
+var version = "1.0.0"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-
+	
 	Short: "Print the version number of the application",
 	Long:  `Print the version number of the application`,
 	Run: func(cmd *cobra.Command, args []string) {
